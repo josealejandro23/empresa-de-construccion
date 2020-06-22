fba4d0ceef9d1f2a5f70ed090f4f904aaf9a709f
Ing. Jose Alejandro Vargas <josevargu@gmail.com>
2020-06-22 17:56:39 -0500

    añadido efecto hover sobre imágenes del area proyectos

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,35 @@ let x = $(document);
 x.ready(inicio);
 
 function inicio() {
-	let item = $(".item"); 
-	item.hover(ingreso, salida); /* ? añade estilos al hacer hover sobre el owl carousel */
+	let item = $(".item");
+	item.hover(
+		ingreso,
+		salida
+	); /* ? añade estilos al hacer hover sobre el owl carousel */
+	let img = $(".img-proyecto");
+	img.hover(ingresoImg, salidaImg);
 }
 
 function ingreso() {
 	$(this).find("h3").removeClass("titulo-hoverOut");
-	$('img',this).removeClass('img-out');
+	$("img", this).removeClass("img-out");
 	$(this).find("h3").addClass("titulo-hoverIn");
-	$('img',this).addClass('img-in');
+	$("img", this).addClass("img-in");
 }
 
 function salida() {
 	$("h3", this).removeClass("titulo-hoverIn");
-	$('img',this).removeClass('img-in');
+	$("img", this).removeClass("img-in");
 	$(this).children("h3").addClass("titulo-hoverOut");
-	$('img',this).addClass('img-out');
+	$("img", this).addClass("img-out");
+}
+
+function ingresoImg() {
+	$("img", this).removeClass("imgPro-out");
+	$("img", this).addClass("imgPro-in");
+}
+
+function salidaImg() {
+	$("img", this).removeClass("imgPro-in");
+	$("img", this).addClass("imgPro-out");
 }
